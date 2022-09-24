16ed8e0533d42ea2a64de07cd310b611af24cc90
AMAURY <amauryfranssen@gmail.com>
2022-09-24 22:10:54 +0200

    feat: new css reset file added and templates architecture set

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+from flask import url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -10,3 +12,8 @@ def hello_world():
 @app.route("/<name>")
 def hello(name):
     return f"Hello, {escape(name)}!"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def say_hello(name=None):
+    return render_template('hello.html', name=name)
