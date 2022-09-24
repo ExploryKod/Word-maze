4020d69b45d3f636499b060e1ef23f248d253e66
AMAURY <amauryfranssen@gmail.com>
2022-09-24 23:20:03 +0200

    feat: templates minimal routes and blocks set

--- app.py
+++ app.py
@@ -6,14 +6,11 @@ from markupsafe import escape
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('welcome.html')
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route("/game")
+def game():
+    return render_template('game.html')
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def say_hello(name=None):
-    return render_template('hello.html', name=name)
+
